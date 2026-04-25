71e8416287d5619e3beb0c8bb7f2e6363b597829
Henrique Mizoe <henrique.mizoe@gmail.com>
2026-04-25 14:48:20 -0300

    Autualizando os teste do controlador de barramento para a comparacao

--- testbenchs/tb_bus_controler.v
+++ testbenchs/tb_bus_controler.v
@@ -18,13 +18,21 @@ module bus_controler_tb;
         .c(c),
         .f_inv_op(f_inv_op) , .f_div_zero(f_div_zero) , .f_overflow(f_overflow) , .f_underflow(f_underflow) , .f_inexact(f_inexact),
         .over(over)
-    );    
+    );
+
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk;
+    end
 
     integer i, j, k;
+    reg [31:0]  ce;
+    reg efinv;
     initial begin
         $dumpfile("bus_controler_tb.vcd");
         $dumpvars(0, bus_controler_tb);
 
+        $display("testando reset e busy");
         $display("addr | reset | busy |f_div_zero");
         for(k=0; k<2; k++)begin
             reset=k;
@@ -38,7 +46,54 @@ module bus_controler_tb;
                 end
             end
         end
+
+        busy=1;
+        reset = 0;
+        addr = 3'b100;
+        $display("testando igualdade (addr=100)");
+        $display("    a     |    b     |    c     | f_inv_op");
+        a = 32'h0000_0000;
+        b = 32'h0000_0000;
+        ce = 32'h3F800000;
+        efinv = 0;
+        wait (over == 1'b1);
+        if(c==ce && efinv == f_inv_op) $display(" %h | %h | %h |    %b       PASS", a, b, c, f_inv_op);
+        else $display(" %h | %h | %h |    %b       FAIL", a, b, c, f_inv_op);
         
+        a=32'h7F800001;
+        b=32'hxx0x_xxxx;
+        ce = 32'h0000_0000;
+        efinv = 1;
+        #10
+        wait (over == 1'b1);
+        if(c==ce && efinv == f_inv_op) $display(" %h | %h | %h |    %b       PASS", a, b, c, f_inv_op);
+        else $display(" %h | %h | %h |    %b       FAIL", a, b, c, f_inv_op);
+
+        busy=1;
+        reset = 0;
+        addr = 3'b101;
+        $display("testando menor que (addr=101)");
+        $display("    a     |    b     |    c     | f_inv_op");
+        a = 32'h8000_0000;
+        b = 32'h0000_0000;
+        ce = 32'h00000000;
+        efinv = 0;
+        #10
+        wait (over == 1'b1);
+        if(c==ce && efinv == f_inv_op) $display(" %h | %h | %h |    %b       PASS", a, b, c, f_inv_op);
+        else $display(" %h | %h | %h |    %b       FAIL", a, b, c, f_inv_op);
+        
+        a=32'h7F800001;
+        b=32'hxxxx_xxxx;
+        ce = 32'h0000_0000;
+        efinv = 1;
+        #10
+        wait (over == 1'b1);
+        if(c==ce && efinv == f_inv_op) $display(" %h | %h | %h |    %b       PASS", a, b, c, f_inv_op);
+        else $display(" %h | %h | %h |    %b       FAIL", a, b, c, f_inv_op);
+
+
+        #10
         $finish;
     end
 
